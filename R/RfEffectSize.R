669459bacc4c39a81714078ef10587a3156f94e7
Tommy Jones <jones.thos.w@gmail.com>
2016-01-29 12:27:00 +0100

    I think this works now...

--- R/RfEffectSize.R
+++ R/RfEffectSize.R
@@ -18,7 +18,62 @@ RfEffectSize <- function(forest, training_data, varlist = NULL, ...){
   # Get the proper parameters for re-training
   cat_classes <- c("factor", "character", "logical", "ordered")
 
-  #### Declare a bunch of functions that we're going to need -----------------
+  #### Declare a bunch of functions that we're going to need -------------------
+
+  # Function for handeling errors (somewhat) elegantly -------------------------
+  HandleError <- function(variable, level_name, error_type = c(1, 2)){
+    if(! error_type %in% c(1, 2) | length(error_type) > 1){
+      stop("You put in an error type that doesn't exist chucklehead. Fix yer code.")
+    }
+
+    # Cases:
+    # 1. There is litte to no variability in y
+    if(error_type == 1){
+      warning(paste("'y' has little or no variability.",
+                    "Cannot compute marginal effect for",
+                    variable,
+                    "at level",
+                    level_name))
+      result <- list(data.frame(level = paste("'y' has little or no variability.",
+                                              "Cannot compute marginal effect for",
+                                              variable,
+                                              "at level",
+                                              level_name),
+                                effect_size = NA,
+                                p_value = NA,
+                                conf_95_low = NA,
+                                conf_95_high = NA,
+                                stringsAsFactors = FALSE))
+      names(result) <- "response"
+
+      return(result)
+    }
+
+    # 2. There is little to no variability in x
+    if(error_type == 2){
+      warning(paste(variable,
+                    "has little or no variability at level",
+                    level_name,
+                    "Cannot compute marginal effects."))
+
+      result <- list(data.frame(level=paste(variable,
+                                            "has little or no variability at level",
+                                            level_name,
+                                            "Cannot compute marginal effects."),
+                                effect_size=NA,
+                                p_value=NA,
+                                conf_95_low=NA,
+                                conf_95_high=NA,
+                                stringsAsFactors=FALSE))
+
+      names(result) <- "response"
+
+      return(result)
+    }
+
+  }
+
+  # Function for actually doing the comparison that gets you effect size -------
   DoComparison <- function(p1, p2, level_name){
     if("matrix" %in% class(p1)){
       result <- lapply(1:ncol(p1), function(j){
@@ -42,7 +97,7 @@ RfEffectSize <- function(forest, training_data, varlist = NULL, ...){
 
       result <- list(response=data.frame(level=level_name,
                                          effect_size=effect_size,
-                                         p_values=test$p.value,
+                                         p_value=test$p.value,
                                          conf_95_low=test$conf.int[ 1 ],
                                          conf_95_high=test$conf.int[ 2 ],
                                          stringsAsFactors=FALSE))
@@ -51,7 +106,7 @@ RfEffectSize <- function(forest, training_data, varlist = NULL, ...){
     return(result)
   }
 
-
+  # Function for continuous predictor variables --------------------------------
   ExecuteContinuous <- function(variable, training_data, varlist, forest, cat_classes){
     # split data above and below the mean
     var_mean <- mean(training_data[[ variable ]])
@@ -71,57 +126,32 @@ RfEffectSize <- function(forest, training_data, varlist = NULL, ...){
 
     # If there is little or no variability in the variable, exit early
     if(nrow(d1) < 5 | nrow(d2) < 5){
-      warning(paste("variable", variable, "has little or no variability. Cannot compute marginal effects."))
-
-      result <- list(data.frame(level=paste("variable", variable,
-                                            "has little or no variability at level",
-                                            level_name,
-                                            "Cannot compute marginal effects."),
-                                effect_size=NA,
-                                p_values=NA,
-                                conf_95_low=NA,
-                                conf_95_high=NA,
-                                stringsAsFactors=FALSE))
-
-      names(result) <- "response"
-
-      return(result)
+      if(forest$type == "classification"){
+        result <- HandleError(variable = variable, level_name = level_name, error_type = 2)
+        result <- lapply(levels(forest$y), function(x){
+          result[[ 1 ]]
+        })
+        names(result) <- levels(forest$y)
+        return(result)
+      }else{
+        result <- HandleError(variable = variable, level_name = level_name, error_type = 2)
+        return(result)
+      }
     }
 
     # If there is little or no variability in the y variable, exit early
-    if(forest$type == "classification"){
+    if(forest$type == "classification"){ # y is discrete
       if(length(unique(d1$y)) == 1 | length(unique(d2$y)) == 1){
-        warning(paste("'y' has little or no variability. Cannot compute marginal effects for variable",
-                      variable, "at level", level_name))
-
-        result <- list(data.frame(level=paste("'y' has little or no variability. Cannot compute marginal effects for variable",
-                                              variable, "at level", level_name),
-                                  effect_size=NA,
-                                  p_values=NA,
-                                  conf_95_low=NA,
-                                  conf_95_high=NA,
-                                  stringsAsFactors=FALSE))
-
-        names(result) <- "response"
-
+        result <- HandleError(variable = variable, level_name = level_name, error_type = 2)
+        result <- lapply(levels(forest$y), function(x){
+          result[[ 1 ]]
+        })
+        names(result) <- levels(forest$y)
         return(result)
       }else{
-        if(var(d1$y) == 0 | var(d2$y) == 0){
-          warning(paste("'y' has little or no variability. Cannot compute marginal effects for variable",
-                        variable, "at level", level_name))
-
-          result <- list(data.frame(level=paste("'y' has little or no variability. Cannot compute marginal effects for variable",
-                                                variable, "at level", level_name),
-                                    effect_size=NA,
-                                    p_values=NA,
-                                    conf_95_low=NA,
-                                    conf_95_high=NA,
-                                    stringsAsFactors=FALSE))
-
-          names(result) <- "response"
-
+        if(var(d1$y) == 0 | var(d2$y) == 0){ # y is continuous
+          result <- HandleError(variable = variable, level_name = level_name, error_type = 1)
           return(result)
-
         }
       }
     }
@@ -156,6 +186,7 @@ RfEffectSize <- function(forest, training_data, varlist = NULL, ...){
     result
   }
 
+  # Function for binary discrete predictor variables ---------------------------
   ExecuteBinary <- function(variable, training_data, varlist, forest, cat_classes){
     # get levels and split data for each level
     var_levels <- as.character(sort(unique(training_data[[ variable ]])))
@@ -171,73 +202,46 @@ RfEffectSize <- function(forest, training_data, varlist = NULL, ...){
     d2 <- cbind(d2, y=forest$y[ ! sub ])
     d2 <- d2[ , c("y", setdiff(varlist, variable)) ]
 
+    # If there is little or no variability in the variable, exit early
     if(nrow(d1) < 5 | nrow(d2) < 5){
-      warning(paste("variable", variable, "has little or no variability. Cannot compute marginal effects."))
-
-      y_lvls <- as.character(sort(unique(forest$y)))
-
-      result <- lapply(y_lvls, function(k){
-        data.frame(level=paste("Cannot compute marginal effects for variable",
-                               variable, "at level", var_levels[ 1 ]),
-                   effect_size=NA,
-                   p_values=NA,
-                   conf_95_low=NA,
-                   conf_95_high=NA,
-                   stringsAsFactors=FALSE)
-      })
-
-      names(result) <- y_lvls
-
-      return(result)
-    }
-
-    # If there is little or no variability in the y variable, exit early
-    if(forest$type == "classification"){
-      if(length(unique(d1$y)) == 1 | length(unique(d2$y)) == 1){
-        warning(paste("'y' has little or no variability. Cannot compute marginal effects for variable",
-                      variable))
-
-        y_lvls <- as.character(sort(unique(forest$y)))
-
-        result <- lapply(y_lvls, function(k){
-          data.frame(level=paste("'y' has little or no variability. Cannot compute marginal effects for variable",
-                                 variable),
-                     effect_size=NA,
-                     p_values=NA,
-                     conf_95_low=NA,
-                     conf_95_high=NA,
-                     stringsAsFactors=FALSE)
-        })
-
-        names(result) <- y_lvls
-
-        return(result)
-
-      }else{
-        if(var(d1$y) == 0 | var(d2$y) == 0){
-          warning(paste("'y' has little or no variability. Cannot compute marginal effects for variable",
-                        variable))
-
-          y_lvls <- as.character(sort(unique(forest$y)))
-
-          result <- lapply(y_lvls, function(k){
-            data.frame(level=paste("'y' has little or no variability. Cannot compute marginal effects for variable",
-                                   variable),
-                       effect_size=NA,
-                       p_values=NA,
-                       conf_95_low=NA,
-                       conf_95_high=NA,
-                       stringsAsFactors=FALSE)
+      if(forest$type == "classification"){
+        result <- lapply(var_levels, function(j){
+          r <- lapply(levels(forest$y), function(k){
+            HandleError(variable = variable, level_name = j, error_type = 2)[[ 1 ]]
           })
-
-          names(result) <- y_lvls
-
-          return(result)
-
-        }
+          names(r) <- levels(forest$y)
+          r
+        })
+        names(result) <- var_levels
+        return(result)
+      }else{
+        result <- lapply(var_levels, function(j){
+          HandleError(variable = variable, level_name = j, error_type = 2)
+        })
+        names(result) <- var_levels
+        return(result)
       }
     }
 
+    # If there is little or no variability in the y variable, exit early
+    if(forest$type == "classification"){ # y is discrete
+      if(length(unique(d1$y)) == 1 | length(unique(d2$y)) == 1){
+        result <- lapply(var_levels, function(j){
+          r <- lapply(levels(forest$y), function(k){
+            HandleError(variable = variable, level_name = j, error_type = 2)[[ 1 ]]
+          })
+          names(r) <- levels(forest$y)
+          r
+        })
+        names(result) <- var_levels
+        return(result)
+      }else{
+        if(var(d1$y) == 0 | var(d2$y) == 0){ # y is continuous
+          result <- HandleError(variable = variable, level_name = var_levels[ 1 ], error_type = 1)
+          return(result)
+        }
+      }
+    }
 
     # Make a formula
     f <- paste("y ~ ",
@@ -269,6 +273,7 @@ RfEffectSize <- function(forest, training_data, varlist = NULL, ...){
 
   }
 
+  # Function for multinomial discrete predictor variables ----------------------
   ExecuteMulti <- function(variable, training_data, varlist, forest, cat_classes){
     var_levels <- as.character(sort(unique(training_data[[ variable ]])))
 
@@ -285,77 +290,34 @@ RfEffectSize <- function(forest, training_data, varlist = NULL, ...){
       d2 <- d2[ , c("y", setdiff(varlist, variable)) ]
 
       # If there is little or no variability in the variable, exit early
+      # If there is little or no variability in the variable, exit early
       if(nrow(d1) < 5 | nrow(d2) < 5){
-        warning(paste("variable", variable, "has little or no variability. Cannot compute marginal effects."))
-
-        y_lvls <- as.character(sort(unique(forest$y)))
-
-        result <- lapply(y_lvls, function(k){
-          data.frame(level=paste("'y' has little or no variability. Cannot compute marginal effects for variable",
-                                 variable),
-                     effect_size=NA,
-                     p_values=NA,
-                     conf_95_low=NA,
-                     conf_95_high=NA,
-                     stringsAsFactors=FALSE)
-        })
-
-        names(result) <- y_lvls
-
-        return(result)
+        if(forest$type == "classification"){
+          result <- HandleError(variable = variable, level_name = x, error_type = 2)
+          result <- lapply(levels(forest$y), function(x){
+            result[[ 1 ]]
+          })
+          names(result) <- levels(forest$y)
+          return(result)
+        }else{
+          result <- HandleError(variable = variable, level_name = x, error_type = 2)
+          return(result)
+        }
       }
 
       # If there is little or no variability in the y variable, exit early
-      if(forest$type == "classification"){
+      if(forest$type == "classification"){ # y is discrete
         if(length(unique(d1$y)) == 1 | length(unique(d2$y)) == 1){
-          warning(paste("'y' has little or no variability. Cannot compute marginal effects for variable",
-                        variable, "at level", x))
-
-          y_lvls <- as.character(sort(unique(forest$y)))
-
-          result <- lapply(y_lvls, function(k){
-            data.frame(level=paste("'y' has little or no variability. Cannot compute marginal effects for variable",
-                                   variable, "at level", x),
-                       effect_size=NA,
-                       p_values=NA,
-                       conf_95_low=NA,
-                       conf_95_high=NA,
-                       stringsAsFactors=FALSE)
+          result <- HandleError(variable = variable, level_name = x, error_type = 2)
+          result <- lapply(levels(forest$y), function(x){
+            result[[ 1 ]]
           })
-
-          names(result) <- y_lvls
-
+          names(result) <- levels(forest$y)
           return(result)
         }else{
-          if(var(d1$y) == 0 | var(d2$y) == 0){
-            warning(paste("'y' has little or no variability. Cannot compute marginal effects for variable",
-                          variable, "at level", x))
-
-            y_lvls <- as.character(sort(unique(forest$y)))
-
-            result <- lapply(y_lvls, function(k){
-              data.frame(level=paste("'y' has little or no variability. Cannot compute marginal effects for variable",
-                                     variable, "at level", x),
-                         effect_size=NA,
-                         p_values=NA,
-                         conf_95_low=NA,
-                         conf_95_high=NA,
-                         stringsAsFactors=FALSE)
-            })
-
-            names(result) <- y_lvls
-            result <- list(data.frame(level=paste("'y' has little or no variability. Cannot compute marginal effects for variable",
-                                                  variable, "at level", x),
-                                      effect_size=NA,
-                                      p_values=NA,
-                                      conf_95_low=NA,
-                                      conf_95_high=NA,
-                                      stringsAsFactors=FALSE))
-
-            names(result) <- x
-
+          if(var(d1$y) == 0 | var(d2$y) == 0){ # y is continuous
+            result <- HandleError(variable = variable, level_name = x, error_type = 1)
             return(result)
-
           }
         }
       }
@@ -387,6 +349,7 @@ RfEffectSize <- function(forest, training_data, varlist = NULL, ...){
     result
   }
 
+  # Wrapper function for running the procedure ---------------------------------
   Execute <- function(variable, training_data, varlist, forest, cat_classes){
     var_class <- class(training_data[[ variable ]])
 
@@ -420,13 +383,13 @@ RfEffectSize <- function(forest, training_data, varlist = NULL, ...){
   #### Get effect size for variables -----------------------------------------
 
 
-  #     effect_sizes <- parallel::mclapply(varlist, function(variable){
-  #         Execute(variable = variable,
-  #                 training_data = training_data,
-  #                 varlist = varlist,
-  #                 forest = forest,
-  #                 cat_classes = cat_classes)
-  #     }, mc.cores = parallel::detectCores())
+      effect_sizes <- parallel::mclapply(varlist, function(variable){
+          Execute(variable = variable,
+                  training_data = training_data,
+                  varlist = varlist,
+                  forest = forest,
+                  cat_classes = cat_classes)
+      }, mc.cores = parallel::detectCores())
 
   #     effect_sizes <- lapply(varlist[1:5], function(variable){
   #       Execute(variable = variable,
@@ -439,9 +402,9 @@ RfEffectSize <- function(forest, training_data, varlist = NULL, ...){
   names(effect_sizes) <- varlist
 
   #### Reformat result for return --------------------------------------------
-  if(class(forest$y) %in% cat_classes){
+  if(forest$type == "classification"){
 
-    y_levels <- as.character(sort(unique(forest$y)))
+    y_levels <- levels(forest$y)
 
     names(y_levels) <- y_levels
 
